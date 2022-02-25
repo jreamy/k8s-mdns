36445229dd74cef7e07817dee0fc9309808cb580
Jack Reamy <jreamy17@georgefox.edu>
2022-02-25 08:50:42 -0800

    in cluster config

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,51 +4,21 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"os"
-	"time"
 
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-
-	"sigs.k8s.io/external-dns/pkg/apis/externaldns"
-	"sigs.k8s.io/external-dns/pkg/apis/externaldns/validation"
-	"sigs.k8s.io/external-dns/source"
 )
 
 func main() {
 
-	cfg1, err := rest.InClusterConfig()
-	fmt.Println(cfg1, err)
-
-	cfg := externaldns.NewConfig()
-	if err := cfg.ParseFlags(os.Args[1:]); err != nil {
-		log.Fatalf("flag parsing error: %v", err)
-	}
-	log.Printf("config: %s", cfg)
-
-	if err := validation.ValidateConfig(cfg); err != nil {
-		log.Fatalf("config validation failed: %v", err)
-	}
-
-	g := &source.SingletonClientGenerator{
-		KubeConfig:   cfg.KubeConfig,
-		APIServerURL: cfg.APIServerURL,
-		// If update events are enabled, disable timeout.
-		RequestTimeout: func() time.Duration {
-			if cfg.UpdateEvents {
-				return 0
-			}
-			return cfg.RequestTimeout
-		}(),
-	}
-
-	cli, err := g.KubeClient()
+	cfg, err := rest.InClusterConfig()
 	if err != nil {
-		log.Fatalf("client initialization failed: %v", err)
+		log.Fatal(err.Error())
 	}
 
+	cli := kubernetes.NewForConfigOrDie(cfg)
 	ListServices(context.Background(), cli)
 }
 
